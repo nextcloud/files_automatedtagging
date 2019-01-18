b14df104829732ff3f86c22442db19e33a2fff61
Joas Schilling <coding@schilljs.com>
2019-01-18 15:42:40 +0100

    Also tag files on updating them
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/StorageWrapper.php
+++ lib/StorageWrapper.php
@@ -22,6 +22,7 @@
 namespace OCA\FilesAutomatedTagging;
 
 use OC\Files\Storage\Wrapper\Wrapper;
+use OCP\Files\Storage\IStorage;
 
 class StorageWrapper extends Wrapper {
 
@@ -55,5 +56,96 @@ class StorageWrapper extends Wrapper {
 		$cache = $this->storage->getCache($path, $storage);
 
 		return new CacheWrapper($cache, $storage, $this->operation, $this->mountPoint);
+	}
+
+	/**
+	 * see http://php.net/manual/en/function.file-put-contents.php
+	 *
+	 * @param string $path
+	 * @param string $data
+	 * @return bool
+	 */
+	public function file_put_contents($path, $data) {
+		$result = $this->storage->file_put_contents($path, $data);
+		if ($result !== false) {
+			$this->checkOperationsOnPath($path);
+		}
+		return $result;
+	}
+
+	/**
+	 * see http://php.net/manual/en/function.fopen.php
+	 *
+	 * @param string $path
+	 * @param string $mode
+	 * @return resource
+	 */
+	public function fopen($path, $mode) {
+		$resource = $this->storage->fopen($path, $mode);
+		if ($mode !== 'r' && $resource !== false) {
+			$this->checkOperationsOnPath($path);
+		}
+		return $resource;
+	}
+
+	/**
+	 * @param IStorage $sourceStorage
+	 * @param string $sourceInternalPath
+	 * @param string $targetInternalPath
+	 * @return bool
+	 */
+	public function moveFromStorage(IStorage $sourceStorage, $sourceInternalPath, $targetInternalPath) {
+		if ($sourceStorage === $this) {
+			return $this->rename($sourceInternalPath, $targetInternalPath);
+		}
+
+		$this->checkOperationsOnPath($targetInternalPath);
+		return $this->storage->moveFromStorage($sourceStorage, $sourceInternalPath, $targetInternalPath);
+	}
+
+	/**
+	 * see http://php.net/manual/en/function.rename.php
+	 *
+	 * @param string $path1
+	 * @param string $path2
+	 * @return bool
+	 */
+	public function rename($path1, $path2) {
+		// Renaming a part file (files/test.txt.ocTransferId1906445132.part => files/test.txt)
+		// mean we update or upload a file.
+		$p = strpos($path1, $path2);
+		if ($p === 0) {
+			$part = substr($path1, strlen($path2));
+			//This is a rename of the transfer file to the original file
+			if (strpos($part, '.ocTransferId') === 0) {
+				$this->checkOperationsOnPath($path2);
+			}
+		}
+
+		return $this->storage->rename($path1, $path2);
+	}
+
+	protected function checkOperationsOnPath(string $path) {
+		if (!$this->isTaggingPath($path)) {
+			return;
+		}
+
+		$fileId = $this->getCache()->getId($path);
+		if ($fileId !== -1) {
+			$this->operation->checkOperations($this, $fileId, $path);
+		}
+	}
+
+	protected function isTaggingPath(string $path): bool {
+		$path = $this->mountPoint . $path;
+
+		if (substr_count($path, '/') < 3) {
+			return false;
+		}
+
+		// '', admin, 'files', 'path/to/file.txt'
+		list(,, $folder,) = explode('/', $path, 4);
+
+		return $folder === 'files';
 	}
 }
