89da4081a2752e628e395cb8a76bee658d42f1eb
Julius Härtl <jus@bitgrid.net>
2019-11-19 08:22:38 +0100

    Bump package versions
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- src/Tag.vue
+++ src/Tag.vue
@@ -25,7 +25,7 @@
 </template>
 
 <script>
-import MultiselectTags from 'nextcloud-vue/dist/Components/MultiselectTags'
+import MultiselectTags from '@nextcloud/vue/dist/Components/MultiselectTags'
 
 export default {
 	name: 'Tag',
