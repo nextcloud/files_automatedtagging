3b51c20f8f0b6be796590e6287fd89f5279de4e2
Arthur Schiwon <blizzz@arthur-schiwon.de>
2020-01-15 15:47:12 +0100

    fix icon color to not be black on black when dark mode is enabled
    Signed-off-by: Arthur Schiwon <blizzz@arthur-schiwon.de>

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ window.OCA.WorkflowEngine.registerOperator({
 	id: 'OCA\\FilesAutomatedTagging\\Operation',
 	name: t('files_automatedtagging', 'Tag a file'),
 	description: t('files_automatedtagging'),
-	iconClass: 'icon-tag-white',
 	color: 'var(--color-success)',
 	operation: '',
 	options: Tag,
