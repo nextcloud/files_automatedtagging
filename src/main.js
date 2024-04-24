b42571328294194c3518e87ab6d65f7f77807e0c
Andy Scherzinger <info@andy-scherzinger.de>
2024-04-24 12:18:00 +0200

    chore: moved the license header to the top of the file
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import Tag from './Tag.vue'
-
 /**
  * SPDX-FileCopyrightText: 2019-2022 Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
+
+import Tag from './Tag.vue'
 
 window.OCA.WorkflowEngine.registerOperator({
 	id: 'OCA\\FilesAutomatedTagging\\Operation',
