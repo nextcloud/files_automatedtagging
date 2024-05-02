bf221c4b74e5524cd747510b99d7214897f7dee3
Joas Schilling <coding@schilljs.com>
2024-05-02 10:42:28 +0200

    fix: Fix strict types
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Operation.php
+++ lib/Operation.php
@@ -89,7 +89,7 @@ class Operation implements ISpecificOperation, IComplexOperation {
 		$matches = $matcher->getFlows(false);
 
 		foreach ($matches as $match) {
-			$this->objectMapper->assignTags($fileId, 'files', explode(',', $match['operation']));
+			$this->objectMapper->assignTags((string) $fileId, 'files', explode(',', $match['operation']));
 		}
 	}
 
