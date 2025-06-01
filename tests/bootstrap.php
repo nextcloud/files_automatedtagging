3ac0cfafe8e56a546b3dd614251236a37e014b28
Joas Schilling <coding@schilljs.com>
2025-06-01 16:02:31 +0200

    ci: Update bootstrap for Server master change
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,23 +1,21 @@
 <?php
 
 declare(strict_types=1);
+
 /**
  * SPDX-FileCopyrightText: 2016 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
+
+use OCP\App\IAppManager;
+use OCP\Server;
 
 if (!defined('PHPUNIT_RUN')) {
 	define('PHPUNIT_RUN', 1);
 }
 
 require_once __DIR__ . '/../../../lib/base.php';
+require_once __DIR__ . '/../../../tests/autoload.php';
 
-if (!class_exists('PHPUnit\Framework\TestCase')) {
-	require_once('PHPUnit/Autoload.php');
-}
-
-// Fix for "Autoload path not allowed: .../files_automatedtagging/tests/..."
-\OC_App::loadApp('files_external');
-\OC_App::loadApp('files_automatedtagging');
-
-OC_Hook::clear();
+Server::get(IAppManager::class)->loadApp('files_external');
+Server::get(IAppManager::class)->loadApp('files_automatedtagging');
